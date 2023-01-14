ceceeb9eb948373064f0860ab2f702f9aa206b39
Siam11651 <siam11651@outlook.com>
2023-01-15 01:08:57 +0600

    initial commit

---
+++ app/PhoneNumberUploader.java
@@ -0,0 +1,16 @@
+import android.content.DialogInterface;
+
+public class PhoneNumberUploader implements DialogInterface
+{
+    @Override
+    public void cancel()
+    {
+
+    }
+
+    @Override
+    public void dismiss()
+    {
+
+    }
+}
